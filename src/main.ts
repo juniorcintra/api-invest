7b582b0b2f0c511ec860c5cc755577ad18fc7962
junioricntra <junior.cintra.developer@gmail.com>
2024-10-18 13:40:59 -0300

    default port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
 
